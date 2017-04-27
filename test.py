772eb438b18a833b59577a51ad22c600be01584c
Marcin Wierciak <marcin.f.w@gmail.com>
2017-04-27 09:41:37 +0200

    5

--- test.py
+++ test.py
@@ -5,9 +5,17 @@ def introduce():
     print("Hello, hello!!!!!!!!!!!")
 
 
+<<<<<<< Updated upstream
 def shout():
     print("AAAAA")
 
 
+=======
+>>>>>>> Stashed changes
 def damdamdam():
     print("Heloo, helloo!!")
+
+def shout()
+    print("shout shout nananna!")
+
+    
